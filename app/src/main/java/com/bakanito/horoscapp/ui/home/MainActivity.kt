fc67524b9aea12592608ec2051147a2cf2131ab7
Javier Garralón Mourín <jgarralon@ayesa.es>
2024-03-06 11:35:14 +0100

    first commit

---
+++ app/src/main/java/com/bakanito/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.bakanito.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.bakanito.horoscapp.R
+import com.bakanito.horoscapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+private lateinit var navController: NavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI(){
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
